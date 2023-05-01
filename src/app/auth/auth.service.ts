ad2eed6d8620a0f1cd250a1d43d6d072a466d097
jardel88 <jardelsilvagomes@hotmail.com>
2023-05-01 16:01:27 -0300

    Using BehaviorSubjects

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
+import { BehaviorSubject, tap } from 'rxjs';
 
 interface UsernameAvailableResponse {
   available: true
@@ -21,6 +22,7 @@ interface SignupResponse {
 export class AuthService {
 
   baseUrl = 'https://api.angular-email.com';
+  signedin$ = new BehaviorSubject(false);
 
   constructor(private http: HttpClient) { }
 
@@ -32,6 +34,10 @@ export class AuthService {
   signup(credentials: SignupCredentials) {
     return this.http.post<SignupResponse>(
       `${this.baseUrl}/auth/signup`, credentials
+    ).pipe(
+      tap(() => {
+        this.signedin$.next(true)
+      })
     )
   }
 }
