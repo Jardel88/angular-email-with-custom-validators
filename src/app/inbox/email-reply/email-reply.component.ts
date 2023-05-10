493f8716250c2e2cc0ba922e2c5338b1d4ff7d3d
jardel88 <jardelsilvagomes@hotmail.com>
2023-05-10 13:52:49 -0300

    Replying to emails

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Email } from '../email';
 
 @Component({
   selector: 'app-email-reply',
   templateUrl: './email-reply.component.html',
   styleUrls: ['./email-reply.component.css']
 })
-export class EmailReplyComponent {
+export class EmailReplyComponent implements OnInit{
+  showModal = false;
+  @Input() email!: Email;
+
+  ngOnInit() {
+    const text = this.email.text.replace(/\n/, '\n> ')
+
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n-------- ${this.email.from} wrote:\n> ${text}`
+    };
+  }
+
+  onSubmit(email: Email) {
+
+  }
 
 }
